93405f11cca75d0e0cf87c52c42e7abc5b046d3a
Ian Wensink <ianwensink@gmail.com>
2019-07-27 14:15:23 +0200

    ci: setup

---
+++ src/server/index.ts
@@ -0,0 +1,39 @@
+import nextJS from 'next';
+import express from 'express';
+import compression from 'compression';
+import slashes from 'connect-slashes';
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const app = nextJS({
+  dev: isProd,
+  dir: './src',
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server
+    .use([compression(), slashes()])
+    .get('/*', async (req: any, res: any) => {
+      if (!req.path.startsWith('/_next/') && !req.path.startsWith('/static/')) {
+        app.render(req, res, '/', {
+          isProd,
+        } as any);
+      } else {
+        handle(req, res);
+      }
+    })
+    // @ts-ignore
+    .listen(port, (err: any) => {
+      if (err) throw err;
+      // eslint-disable-next-line no-console
+      console.info(`> Ready on http://localhost:${port}`);
+    });
+});
